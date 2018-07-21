b8787f57ab6c9f034d73d0dc8a08ad2754c87f06
Wang Guan <momocraft@gmail.com>
2018-07-21 20:55:52 +0900

    do something

--- src/component/my-page.tsx
+++ src/component/my-page.tsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import { BasePage } from "./base-page";
 import { connectStore } from "./connect-store";
+import { Button } from "@material-ui/core";
 
 class MyPageView extends BasePage<{}> {
   render() {
-    console.log(this.props);
-
     return (
-      <div>
-        MYPAGE
+      <div id="my-page">
+        <Button />
       </div>
     );
   }
